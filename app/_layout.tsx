d490fb146a7e68c0cc52a77fca416f14606bb149
danieldave <danielolusoji@gmail.com>
2024-11-27 01:07:05 +0100

    Full Commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,29 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ScreenStack } from 'react-native-screens';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import OnboardingScreen from '@/components/OnboardingScreen';
+import HomeScreen from '@/components/HomeScreen';
+import index from '.././app/(tabs)'
+import modal from './modal'
+import { getItem } from '@/utils/asyncStorage';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Define the types for the screens in your navigator
+export type RootParamList = {
+  Home: undefined;
+  Onboarding: undefined; // Or specify any params if needed
+};
+
+const Stack = createStackNavigator<RootParamList>();
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -48,12 +65,58 @@ export default function RootLayout() {
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+  const Stack = createNativeStackNavigator();
+  //added boolean to make sure the state true and false can be accessed later in the code
+  const [showOnboarding, setShowOnBoarding] = useState<null | boolean>(null);
+  useEffect(() => {
+    checkIfAlreadyOnboarded();
+  }, [])
+
+  const checkIfAlreadyOnboarded = async () => {
+    let onboarded = await getItem('onboarded');
+    //used Number(onboarded) === 1 for typescriupt to understand it is not string
+    if (Number(onboarded) === 1) {
+      //hide something
+      setShowOnBoarding(false);
+    } else {
+      //show something
+      setShowOnBoarding(true);
+    }
+  }
+
+  if (showOnboarding == null) {
+    return null;
+  }
+
+
+  if (showOnboarding) {
+    return (
+      <NavigationContainer independent={true}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack.Navigator initialRouteName='Onboarding'>
+            <Stack.Screen name="(tabs)" component={index} options={{ headerShown: false }} />
+            <Stack.Screen name="modal" component={modal} options={{ presentation: 'modal' }} />
+            <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            {/* <Stack.Screen name="modal" component={modal} options={{ presentation: 'modal' }} /> */}
+          </Stack.Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer independent={true}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name="(tabs)" component={index} options={{ headerShown: false }} />
+            <Stack.Screen name="modal" component={modal} options={{ presentation: 'modal' }} />
+            <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            {/* <Stack.Screen name="modal" component={modal} options={{ presentation: 'modal' }} /> */}
+          </Stack.Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
+    );
+  }
+
 }
